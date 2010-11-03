277167d10b15b4689f248f9a00f41abfe9a7aa76
Jonas Myrlund <myrlund@gmail.com>
2010-11-03 12:04:42 +0100

    Sophisticated bootstrap.sh a little.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-git clone git://github.com/LearnBoost/Socket.IO-node.git server/socket.io
+if [ -d server/socket.io ]; then
+	pushd server/socket.io
+	git pull origin master
+	popd
+else
+	git clone git://github.com/LearnBoost/Socket.IO-node.git server/socket.io
+fi
